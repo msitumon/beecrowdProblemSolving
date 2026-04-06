a2ef941cc44859ba7cab38878d0167d905da7bc1
Samiul Islam Tumon <msitumon2@gmail.com>
2026-04-07 03:09:00 +0600

    Sequence IJ2

---
+++ 1096.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    //Sequence IJ2
+    int i,j,a,b;
+    for(i=1; i<=9; i+=2){
+        printf("I=%d J=%d\n",i,7);
+        printf("I=%d J=%d\n",i,6);
+        printf("I=%d J=%d\n",i,5);
+    }
+return 0;
+}
