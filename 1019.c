2f8dbdb8dd7293b542a2e6fc8bd6008fd5b4fcd6
Samiul Islam Tumon <msitumon2@gmail.com>
2026-04-17 02:26:59 +0600

    Time conversion.

---
+++ 1019.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    //Time conversion.
+    int N,hours,minutes,seconds;
+    scanf("%d",&N);
+    hours=N/3600;
+    N=N%3600;
+    minutes=N/60;
+    N=N%60;
+    seconds=N;
+    printf("%d:%d:%d\n",hours,minutes,seconds);
+}
